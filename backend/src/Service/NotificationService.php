fbdbc450673981b3d97b5a7c9da58f2721c9b358
Anton Dachauer <ad@var-lab.com>
2025-05-11 22:43:28 +0200

    Email notifications

---
+++ backend/src/Service/NotificationService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Service;
+
+class NotificationService
+{
+
+    public function sendNotification(
+        string $subject,
+        string $message,
+        string $recipientEmail,
+    ): void
+    {
+
+    }
+
+}
