45dc171026c30f2d101d4a26c20a14efde365fb8
Anton Dachauer <ad@var-lab.com>
2025-05-16 10:35:58 +0200

    fix

--- frontend/tests/components/user/AppHeader.spec.ts
+++ frontend/tests/components/user/AppHeader.spec.ts
@@ -1,6 +1,6 @@
 import {describe, expect, test} from "vitest";
 import {render} from "vitest-browser-vue";
-import Header from "../../../src/components/user/Header.vue";
+import AppHeader from "../../../src/components/user/AppHeader.vue";
 import {User} from "../../../src/types/User";
 
 const userMock: User = {
@@ -14,7 +14,7 @@ const userMock: User = {
 
 describe('header', () => {
     test('renders the header component', async () => {
-        const result = render(Header, {
+        const result = render(AppHeader, {
             props: {
                 user: userMock,
             },
